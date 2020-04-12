864a9a01f7b336114272960a2ad8cbd4adc82bf6
Adrian <adriandz@spinmaster.com>
2020-04-12 16:43:48 -0400

    added build folder

---
+++ build/precache-manifest.50c3fa7350366325021d37852cde298f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b8651677a224375f9a73085f89cd36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed0e6590366122be1908",
+    "url": "/static/css/1.8cb03020.chunk.css"
+  },
+  {
+    "revision": "f411e5bab37152a463cc",
+    "url": "/static/css/5.d735a7f6.chunk.css"
+  },
+  {
+    "revision": "b4d0c864403662fa2692",
+    "url": "/static/css/main.35f90d70.chunk.css"
+  },
+  {
+    "revision": "ec209f05f0c7caeb3f06",
+    "url": "/static/js/0.1734a08d.chunk.js"
+  },
+  {
+    "revision": "b4e9bfab70e3172dde3f42e6c2f1f700",
+    "url": "/static/js/0.1734a08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0e6590366122be1908",
+    "url": "/static/js/1.e4fb6fcb.chunk.js"
+  },
+  {
+    "revision": "912373db1b8372d77963",
+    "url": "/static/js/4.a71517f2.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/4.a71517f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f411e5bab37152a463cc",
+    "url": "/static/js/5.8d92583b.chunk.js"
+  },
+  {
+    "revision": "b4d0c864403662fa2692",
+    "url": "/static/js/main.1fa0ffcd.chunk.js"
+  },
+  {
+    "revision": "c53a21f5161ba910314e",
+    "url": "/static/js/runtime-main.414cea03.js"
+  }
+]);
